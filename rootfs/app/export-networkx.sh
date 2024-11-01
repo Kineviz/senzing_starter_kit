5db657a0f7cf3b1a0beaee5c0246aa7f183ae6e8
docktermj <michael@dockter.com>
2024-11-01 12:35:43 -0400

    #1 Savepoint

--- rootfs/app/export-networkx.sh
+++ rootfs/app/export-networkx.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/env bash
+
+set -x
+
+echo "y" | G2ConfigTool.py -f /config/demo-config.g2c
+
+/app/file-loader.py -f /data/open-ownership.json
+/app/file-loader.py -f /data/open-sanctions.json
+
+/app/sz_graph_export.py -S -o /export/demo_
